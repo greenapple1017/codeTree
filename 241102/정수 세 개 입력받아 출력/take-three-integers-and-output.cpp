ab219102bec26ffe53a5334ffe1dbd7c3a51e9a9
greenapple1017 <107623032+greenapple1017@users.noreply.github.com>
2024-11-02 23:48:42 +0900

    Title: 정수 세 개 입력받아 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241102/정수 세 개 입력받아 출력/take-three-integers-and-output.cpp
+++ 241102/정수 세 개 입력받아 출력/take-three-integers-and-output.cpp
@@ -4,7 +4,8 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int a,b,c;
-    cin >> a >> b >> c;
-    cout << a << " " << b << endl << c;
+    cin >> a >> b;
+    cin >> c;
+    cout << a << " " << b << " " << c;
     return 0;
 }
