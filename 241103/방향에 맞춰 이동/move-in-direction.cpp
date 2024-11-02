b3a06f0049a47334daeb12a1b16d1fe8304935c2
greenapple1017 <107623032+greenapple1017@users.noreply.github.com>
2024-11-03 01:03:07 +0900

    Title: 방향에 맞춰 이동, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241103/방향에 맞춰 이동/move-in-direction.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int dx[4] = {0,1,0,-1};
+int dy[4] = {1,0,-1,0};
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    int x = y = 0;
+    while (n--) {
+        char dir;
+        int d;
+        int num;
+        cin >> dir >> num;
+        if (dir == 'N') d = 0;
+        else if (dir == 'E') d = 1;
+        else if (dir == 'S') d = 2;
+        else if (dir == 'W') d = 3;
+        while (num--) {
+            x += dx[d];
+            y += dy[d];
+        }
+    }
+    cout << x << " " << y;
+    return 0;
+}
