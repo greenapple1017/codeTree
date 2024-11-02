f15b106bfd254d5227f847cfcfc766ebb2d60c74
greenapple1017 <107623032+greenapple1017@users.noreply.github.com>
2024-11-03 00:58:59 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241103/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void print10stars() {
+    cout << "**********" << '\n';
+    return;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    for (int i = 0; i < 5; i++) {
+        print10stars();
+    }
+
+    return 0;
+}
