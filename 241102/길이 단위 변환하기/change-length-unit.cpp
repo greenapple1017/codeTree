5a31f59cc2e8bd81214cf664148e7356f7c88c2b
greenapple1017 <107623032+greenapple1017@users.noreply.github.com>
2024-11-02 19:16:02 +0900

    Title: 길이 단위 변환하기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 241102/길이 단위 변환하기/change-length-unit.cpp
+++ 241102/길이 단위 변환하기/change-length-unit.cpp
@@ -3,8 +3,11 @@ using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int ft = 30.48, mi = 160934;
-    cout << "9.2ft = " << 9.2*ft << "cm";
+    double ft = 30.48, mi = 160934;
+
+    cout << fixed;
+    cout.precision(1);
+    cout << "9.2ft = " << 9.2*ft << "cm" << endl;
     cout << "1.3mi = " << 1.3*mi << "cm";
     return 0;
 }
