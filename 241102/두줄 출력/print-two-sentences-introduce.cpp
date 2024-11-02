a127e457c58a942c0acc91ca96d0f0dbcb04c47a
greenapple1017 <107623032+greenapple1017@users.noreply.github.com>
2024-11-02 18:35:43 +0900

    Title: 두줄 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241102/두줄 출력/print-two-sentences-introduce.cpp
+++ 241102/두줄 출력/print-two-sentences-introduce.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    cout << "Hello students!" << endl << "Welcome to LeebroCode!";
+    cout << "Hello students!" << endl << "Welcome to LeebrosCode!";
     return 0;
 }
