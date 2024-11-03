dd2d30afae03119342619e8713b5fb5c88203dfd
greenapple1017 <107623032+greenapple1017@users.noreply.github.com>
2024-11-03 19:22:23 +0900

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241103/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void print(int n) {
+    for (int i = 0; i < n; i++) {
+        cout << "12345^&*()_\n";
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    print(n);
+    return 0;
+}
