2ac7ba38669f5338bb4f618fc98af3fa2b099bd5
greenapple1017 <107623032+greenapple1017@users.noreply.github.com>
2024-11-08 23:37:39 +0900

    Title: 간단한 사칙연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241108/간단한 사칙연산/simple-arithmetic-operation.cpp
+++ 241108/간단한 사칙연산/simple-arithmetic-operation.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int a, b;
-    cin >> a >> b << endl;
+    cin >> a >> b;
     cout << a + b << '\n';
     cout << a - b << '\n';
     cout << a / b << '\n';
