7153fd1e10b5455e6ae27f9a781816464b609a8f
greenapple1017 <107623032+greenapple1017@users.noreply.github.com>
2024-11-11 22:29:23 +0900

    Title: 문자 개수 세기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241111/문자 개수 세기/count-char.cpp
+++ 241111/문자 개수 세기/count-char.cpp
@@ -6,7 +6,7 @@ int main() {
     // 여기에 코드를 작성해주세요.
     string str;
     getline(cin, str);
-    string a;
+    char a;
     cin >> a;
     int ans = 0;
     for (int i = 0; i < str.length(); i++) {
