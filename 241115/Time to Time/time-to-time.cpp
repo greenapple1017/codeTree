68abd225e8e033b2665e6d77b76bcf847b398ef4
greenapple1017 <107623032+greenapple1017@users.noreply.github.com>
2024-11-15 23:48:29 +0900

    Title: Time to Time, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241115/Time to Time/time-to-time.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int fromZero(int &h, int &m) {
+    return h * 60 + m;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c, d;
+    cout << fromZero(c,d) - fromZero(a,b);
+
+    return 0;
+}
