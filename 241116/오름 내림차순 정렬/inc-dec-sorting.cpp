19068b8cb8be8ff4911ed2664dd6e26d6369aff0
greenapple1017 <107623032+greenapple1017@users.noreply.github.com>
2024-11-16 10:35:44 +0900

    Title: 오름 내림차순 정렬, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241116/오름 내림차순 정렬/inc-dec-sorting.cpp
+++ 241116/오름 내림차순 정렬/inc-dec-sorting.cpp
@@ -11,9 +11,10 @@ int main() {
     cin >> n;
     for (int i = 0; i < n; i++) cin >> arr[i];
     sort(arr, arr+n);
-    for (int num : arr) cout << num;
+    for (int i = 0; i < n; i++) cout << arr[i] << " ";
+    cout << endl;
     sort(arr, arr+n, greater<int>());
-    for (int num : arr) cout << num;
+    for (int i = 0; i < n; i++) cout << arr[i] << " ";
 
     return 0;
 }
