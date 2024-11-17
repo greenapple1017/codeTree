08489e07b48e59ad82e81bdba9986cfba3aef4ea
greenapple1017 <107623032+greenapple1017@users.noreply.github.com>
2024-11-17 14:07:18 +0900

    Title: n개의 숫자 중 최소, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241117/n개의 숫자 중 최소/min-of-n-num.cpp
+++ 241117/n개의 숫자 중 최소/min-of-n-num.cpp
@@ -14,8 +14,8 @@ int main() {
         cin >> arr[i];
         if (arr[i] < min_val) min_val = arr[i];
     }
-    for (auto i : arr) {
-        if (i == min_val) cnt++;
+    for (auto num : arr) {
+        if (num == min_val) cnt++;
     }
     cout << min_val << " " << cnt;
     return 0;
