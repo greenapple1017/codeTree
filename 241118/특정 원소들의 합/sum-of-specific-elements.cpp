842dc44f36e86c807dce7d8346995a9820587455
greenapple1017 <107623032+greenapple1017@users.noreply.github.com>
2024-11-18 22:31:42 +0900

    Title: 특정 원소들의 합, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241118/특정 원소들의 합/sum-of-specific-elements.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int arr[4][4];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int ans = 0;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            cin >> arr[i][j];
+        }
+    }
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j <= i; j++) {
+            ans += arr[i][j];
+        }
+    }
+    return 0;
+}
