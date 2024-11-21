b6e747c3bad50f445daf0379aaa68d11a0907d30
greenapple1017 <107623032+greenapple1017@users.noreply.github.com>
2024-11-21 22:43:06 +0900

    Title: 정수의 최솟값, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241121/정수의 최솟값/minimum-value-of-an-integer.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void sol(int a, int b, int b) {
+    cout << min(min(a,b),c);
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c;
+    cin >> a >> b >> c;
+    sol(a,b,c);
+    return 0;
+}
