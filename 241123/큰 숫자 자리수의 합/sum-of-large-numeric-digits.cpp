7060bb226281aeb6774d3b7a3d6f99eecc519995
greenapple1017 <107623032+greenapple1017@users.noreply.github.com>
2024-11-23 16:45:48 +0900

    Title: 큰 숫자 자리수의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241123/큰 숫자 자리수의 합/sum-of-large-numeric-digits.cpp
+++ 241123/큰 숫자 자리수의 합/sum-of-large-numeric-digits.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 using namespace std;
 
-void Sum(long long n) {
+long long Sum(long long n) {
     if (n < 10) return n;
 
-    return (n % 10) + Sum(n-1);
+    return (n % 10) + Sum(n / 10);
 }
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int a,b,c;
+    long long a,b,c;
     cin >> a >> b >> c;
     long long mul = a*b*c;
     cout << Sum(mul);
