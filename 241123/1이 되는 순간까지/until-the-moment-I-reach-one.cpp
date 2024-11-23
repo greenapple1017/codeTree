b355e080ff3f268085e96246ee8f04cba608b371
greenapple1017 <107623032+greenapple1017@users.noreply.github.com>
2024-11-23 16:05:45 +0900

    Title: 1이 되는 순간까지, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 241123/1이 되는 순간까지/until-the-moment-I-reach-one.cpp
+++ 241123/1이 되는 순간까지/until-the-moment-I-reach-one.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int ans;
 
+/*
 void DevideNum(int n) {
     if (n == 1) return;
 
@@ -17,11 +18,30 @@ void DevideNum(int n) {
     ans++;
     DevideNum(n);
 }
+*/
+
+// 이 함수는 n이 주어졌을 때 1이 되기까지 나눈 횟수를 return하는 함수이다. 
+int CountDevide(int n) {
+    // n에 1이 주어지면 더 이상 나눌 횟수는 없으므로 0이 답이다. 
+    if (n == 1) {
+        return 0;
+    }
+
+    // 수학적 귀납법
+    if (n % 2 == 0) {
+        return 1 + CountDevide(n / 2);
+    }
+    else {
+        return 1 + CountDevide(n / 3);
+    }
+}
+
 int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     cin >> n;
-    DevideNum(n);
-    cout << ans;
+    //DevideNum(n);
+    //cout << ans;
+    cout << CountDevide(n);
     return 0;
 }
