43629dbfd3f2b445bfe5bc839a31316de34bb332
greenapple1017 <107623032+greenapple1017@users.noreply.github.com>
2024-11-24 18:31:27 +0900

    Title: 일렬로 서있는 소 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241124/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int arr[100];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = i+1; j < n; j++) {
+            for (int k = j+1; k < n; k++) {
+                if (arr[i] <= arr[j] && arr[j] <= arr[k]) ans++;
+            }
+        }
+    }
+    cout << ans;
+    return 0;
+}
