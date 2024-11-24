e1d49efa8898071e9e701996507c13da068bbe3a
greenapple1017 <107623032+greenapple1017@users.noreply.github.com>
2024-11-24 12:27:46 +0900

    Title: 흰검 칠하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241124/흰검 칠하기/painting-white-black.cpp
+++ 241124/흰검 칠하기/painting-white-black.cpp
@@ -4,8 +4,56 @@ using namespace std;
 #define OFFSET 100000
 #define MAX_TILE 200001
 
-int tile[MAX_TILE];
+int tile[MAX_TILE], cnt_tile[MAX_TILE];
 
+int main() {
+    int n;
+    cin >> n;
+    int cur = OFFSET;
+    for (int i = 0; i < n; i++) {
+        int num;
+        char dir;
+        cin >> num >> dir;
+        if (dir == 'R') {
+            for (int j = 0; j < num; j++) {
+                tile[cur+j] = 1; // 1 for black, 0 for white
+                cnt_tile[cur+j]++;
+            }
+            cur += (num-1);
+        }
+        else {
+            for (int j = 0; j < num; j++) {
+                tile[cur-j] = 0; // 1 for black, 0 for white
+                cnt_tile[cur-j]++;
+            }
+            cur -= (num-1);            
+        }
+    }
+    int b = 0, w = 0, g = 0;
+    for (int i = 0; i < MAX_TILE; i++) {
+        if (cnt_tile[i] >= 4) g++;
+        else if (cnt_tile[i] > 0 && cnt_tile[i] < 4) {
+            if (tile[i] == 1) b++;
+            else if (tile[i] == 0) w++;
+        }
+    }
+    cout << w << " " << b << " " << g;
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 int main() {
     // 여기에 코드를 작성해주세요.
     int n;
@@ -44,3 +92,4 @@ int main() {
     cout << white << " " << black << " " << gray;
     return 0;
 }
+*/
