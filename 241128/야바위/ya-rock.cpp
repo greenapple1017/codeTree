fe580c7d511f50c20015e843d62d15af81f083e7
greenapple1017 <107623032+greenapple1017@users.noreply.github.com>
2024-11-28 23:34:28 +0900

    Title: 야바위, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241128/야바위/ya-rock.cpp
+++ 241128/야바위/ya-rock.cpp
@@ -9,20 +9,30 @@ void Swap(int a, int b){
     arr[b] = tmp;
 }
 
+void Print() {
+    for (int i = 1; i <= 3; i++) {
+        cout << arr[i] << ' ';
+    }
+    cout << endl;
+}
+
 int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     cin >> n;
-    arr[n] = 1;
     int ans = 0;
-    for (int k = 0; k < 3; k++) {
+    for (int k = 1; k <= 3; k++) {
+        arr[1] = arr[2] = arr[3] = 0;
         int ans_cand = 0;
+        arr[k] = 1;
         for (int i = 0; i < n; i++) {
             int a, b, c;
             cin >> a >> b >> c;
             Swap(a,b);
             if (arr[c] == 1) ans_cand++;
         }
+        //Print();
+        //cout << "ans_cand: " << ans_cand;
         if (ans_cand > ans) ans = ans_cand;
     }
     cout << ans;
