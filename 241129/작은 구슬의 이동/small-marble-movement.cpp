8950c7336f29d222303c983b78d3f45fa1ba00b2
greenapple1017 <107623032+greenapple1017@users.noreply.github.com>
2024-11-29 23:48:10 +0900

    Title: 작은 구슬의 이동, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241129/작은 구슬의 이동/small-marble-movement.cpp
+++ 241129/작은 구슬의 이동/small-marble-movement.cpp
@@ -3,14 +3,16 @@ using namespace std;
 
 int arr[55][55]; // use index 1 ~ 
 
-int dr[4] = {-1,0,-1,1};
-int dy[4] = {0,1,0,0};
+int dr[4] = {-1,0,0,1};
+int dc[4] = {0,1,-1,0};
 
 struct Ball {
     int r;
     int c;
     int dir;
 };
+
+Ball ball;
 
 int GetDir(char d) {
     if (d == 'U') return 0;
@@ -19,7 +21,7 @@ int GetDir(char d) {
     else if (d == 'L') return 2;
 }
 
-bool ChangeDir() {
+bool ChangeDir(int n) {
     if (ball.r == 1 && ball.dir == 0 || 
         ball.r == n && ball.dir == 3 ||
         ball.c == 1 && ball.dir == 2 ||
@@ -29,6 +31,9 @@ bool ChangeDir() {
         return false;
 }
 
+void Print() {
+    cout << "(r,c): " << '(' << ball.r << ',' << ball.c << ')' << '\n';
+}
 
 int main() {
     // 여기에 코드를 작성해주세요.
@@ -37,17 +42,26 @@ int main() {
     int r, c;
     char d;
     cin >> r >> c >> d;
-    Ball ball{r, c, GetDir(d)};
+    ball.r = r;
+    ball.c = c;
+    ball.dir = GetDir(d);
+
     for (int i = 0; i < t; i++) {
-        if (ChangeDir()) {
+        //cout << "Before Move: " << endl;
+        //Print();
+        if (ChangeDir(n)) {
             ball.dir = 3 - ball.dir;
         }
         else {
             ball.r += dr[ball.dir];
             ball.c += dc[ball.dir];
-        }
+        } 
+        //cout << "After Move: " << endl;
+        //Print();     
     }
+
     cout << ball.r << ' ' << ball.c;
+
 
     return 0;
 }
