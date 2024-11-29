8b148e8d1501e4ab38fb45911c99ab6465ffc0db
greenapple1017 <107623032+greenapple1017@users.noreply.github.com>
2024-11-30 08:50:13 +0900

    Title: 만나는 그 순간, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 241130/만나는 그 순간/the-moment-we-meet.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+int a[1005];
+int b[1005];
+int cur_a, cur_b;
+int t_a, t_b;
+
+void Print() {
+    cout << "cur_a: " << cur_a << '\n';
+    cout << "cur_b: " << cur_b << '\n';
+    cout << "t_a: " << t_a << '\n';
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n,m;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        char d;
+        int t;
+        cin >> d >> t;
+        if (d == 'R') {
+            for (int j = 0; j < t; j++) {
+                cur_a++;
+                t_a++;
+                a[t_a] = cur_a;
+            }
+        }
+        else if (d == 'L') {
+            for (int j = 0; j < t; j++) {
+                cur_a--;
+                t_a++;
+                a[t_a] = cur_a;
+            }
+        }
+        //Print();
+    }
+    for (int i = 0; i < m; i++) {
+        char d;
+        int t;
+        cin >> d >> t;
+        if (d == 'R') {
+            for (int j = 0; j < t; j++) {
+                cur_b++;
+                t_b++;
+                b[t_b] = cur_b;
+            }
+        }
+        else if (d == 'L') {
+            for (int j = 0; j < t; j++) {
+                cur_b--;
+                t_b++;
+                b[t_b] = cur_b;
+            }
+        }
+        //Print();
+    }
+    for (int i = 1; i <= t_a; i++) {
+        if (a[i] == b[i]) {
+            cout << i;
+            return 0;
+        }
+    }
+    cout << -1;
+}
