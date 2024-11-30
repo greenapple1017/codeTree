88e2f7f81a83aedd3e7a20a8f5ab0e47922dd2fd
greenapple1017 <107623032+greenapple1017@users.noreply.github.com>
2024-11-30 21:49:04 +0900

    Title: 구역 청소, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241130/구역 청소/district-cleaning.cpp
+++ 241130/구역 청소/district-cleaning.cpp
@@ -10,17 +10,17 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a, b;
     cin >> a >> b;
-    for (int i = a; i <= b; i++) {
+    for (int i = a; i < b; i++) {
         x_axis[i] = 1;
     }
     int c, d;
     cin >> c >> d;
-    for (int i = c; i <= d; i++) {
+    for (int i = c; i < d; i++) {
         x_axis[i] = 1;
     }
     for (int i = 0; i < MAX_X; i++) {
         if (x_axis[i] != 0) ans++;
     }
-    cout << ans-1;
+    cout << ans;
     return 0;
 }
