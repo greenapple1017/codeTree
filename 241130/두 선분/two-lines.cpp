e8ee99d11f5669cfa1dba3d05d9c06a2f2ea7a35
greenapple1017 <107623032+greenapple1017@users.noreply.github.com>
2024-11-30 21:24:18 +0900

    Title: 두 선분, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241130/두 선분/two-lines.cpp
+++ 241130/두 선분/two-lines.cpp
@@ -6,6 +6,6 @@ int main() {
     int x1, x2, x3, x4;
     cin >> x1 >> x2 >> x3 >> x4;
     if (x2 < x3 || x4 < x1) cout << "nonintersecting";
-    cout << "intersecting";
+    else cout << "intersecting";
     return 0;
 }
