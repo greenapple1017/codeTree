dcefe07443126369f082fe16570353443a241d07
greenapple1017 <107623032+greenapple1017@users.noreply.github.com>
2024-12-02 22:54:27 +0900

    Title: 총점 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241202/총점 비교/compare-total-points.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+struct Student{
+    string name;
+    int kor;
+    int eng;
+    int math;
+};
+
+Student students[15];
+
+bool Cmp(Student &a, Student &b) {
+    return a.kor + a.eng + a.math < b.kor + b.eng + b.math;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> students[i].name
+            >> students[i].kor
+            >> students[i].eng
+            >> students[i].math;
+    }
+    sort(students, students + n, Cmp);
+    for (int i = 0; i < n; i++) {
+        cout << students[i].name << ' '
+            << students[i].kor << ' '
+            << students[i].eng << ' '
+            << students[i].math << '\n';       
+    }
+
+    return 0;
+}
