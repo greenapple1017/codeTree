4ddf82d6c2f1ce5eeda2f5a1450a8a61f3866c5e
greenapple1017 <107623032+greenapple1017@users.noreply.github.com>
2024-12-13 22:15:27 +0900

    Title: 거꾸로 순열, Time: 15ms, Memory: 1MB, Status: Runtime Error - CodeTree

---
+++ 241213/거꾸로 순열/backward-permutation.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int n;
+int arr[8];
+
+void f(int idx) {
+    if (idx == n) {
+        for (auto i : arr) {
+            cout << i << ' ';
+        }
+        cout << '\n';
+    }
+
+    for (int i = n; i > 0; i--) {
+        arr[idx] = i;
+        f(idx+1);
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+    f(0);
+    return 0;
+}
