9623ed58c1fe12ca2aa65fa36e3a100e419779d3
greenapple1017 <107623032+greenapple1017@users.noreply.github.com>
2024-12-16 23:37:05 +0900

    Title: 아스키코드표 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241216/아스키코드표 맞추기/chart-of-ASCII.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b,c,d,e;
+    cin >> a >> b >> c >> d >> e;
+    cout << (char)a << ' ' << (char)b << ' ' << (char)c << ' ' << (char)d << ' ' << (char)e << ' ';
+    return 0;
+}
