d4e918ef734971c9f1ee8da2a1f633b8a332c805
greenapple1017 <107623032+greenapple1017@users.noreply.github.com>
2024-12-17 22:00:57 +0900

    Title: 그 전 알파벳, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241217/그 전 알파벳/before-the-alphabet.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char c;
+    cin >> c;
+    if (c == 'a') {
+        cout << 'z';
+    }
+    else {
+        cout << (char)((int)c - 1);
+    }
+    return 0;
+}
