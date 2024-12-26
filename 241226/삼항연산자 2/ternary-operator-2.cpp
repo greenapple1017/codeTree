201501b29f39d728f2df63981e9f566c7b5c214b
greenapple1017 <107623032+greenapple1017@users.noreply.github.com>
2024-12-26 22:32:38 +0900

    Title: 삼항연산자 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241226/삼항연산자 2/ternary-operator-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    cin >> a;
+    char ans = (a == 1) ? 't' : 'f';
+    cout << ans;
+    return 0;
+}
