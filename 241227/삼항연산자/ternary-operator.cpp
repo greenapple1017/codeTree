d1746b8f5a0da2e2002df6459c293a74a1f4cda9
greenapple1017 <107623032+greenapple1017@users.noreply.github.com>
2024-12-27 23:07:24 +0900

    Title: 삼항연산자, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241227/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    int ans = (n == 100) ? "pass" : "failure";
+    cout << ans;
+    return 0;
+}
