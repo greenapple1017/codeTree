53459d884a2026f7a1b8f958fa175413b32d339c
greenapple1017 <107623032+greenapple1017@users.noreply.github.com>
2024-12-30 23:02:24 +0900

    Title: 두 수의 합, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241230/두 수의 합/sum-of-two-num.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+#define MAX_N 100000
+
+int n, k;
+int ans; 
+unordered_map<int, int> umap;
+
+int main() {
+    cin >> n >> k;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        int complement = k-arr[i];
+        ++umap[complement];
+    }
+    for (int num : arr) {
+        ans += umap[k-num];
+        ans /= 2;
+    }
+    cout << ans;
+}
+
+/*
+arr에 저장.
+보수를 umap에 저장.
+arr 돌면서 보수의 value 곱해서 더하기 
+*/
