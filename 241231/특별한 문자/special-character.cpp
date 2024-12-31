7c55d0bd0d4c5a87c0e39e1ae5a61d20da517ff9
greenapple1017 <107623032+greenapple1017@users.noreply.github.com>
2024-12-31 22:00:42 +0900

    Title: 특별한 문자, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241231/특별한 문자/special-character.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string s;
+    cin >> s;
+    unordered_map<char, int> freq;
+    for (const char &c : s) {
+        ++freq[c];
+    }
+    for (auto it = freq.cbegin(); it != freq.cend(); ++it) {
+        if (it->second == 1) {
+            cout << it->first;
+            exit(0);
+        }
+    }
+    cout << "None";
+    return 0;
+}
