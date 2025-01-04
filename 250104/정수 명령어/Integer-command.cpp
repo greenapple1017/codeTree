fa027ecae13ac4f5d03b1b919a02e3c77ebbefef
greenapple1017 <107623032+greenapple1017@users.noreply.github.com>
2025-01-04 13:31:48 +0900

    Title: 정수 명령어, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250104/정수 명령어/Integer-command.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int t;
+set<int> s;
+
+
+int main() {
+    // Please write your code here.
+    cin >> t;
+    for (int _i = 0; _i < t; _i++) {
+        int k;
+        cin >> k;
+        for (int i = 0; i < k; i++) {
+            char cmd;
+            cin >> cmd;
+            if (cmd == 'I') {
+                int n;
+                cin >> n;
+                s.insert(n);
+            }
+            else {
+                if (!s.empty()) {
+                    int num;
+                    cin >> num;
+                    if (num == 1) {
+                        s.erase(*s.rbegin());
+                    }
+                    else {
+                        s.erase(*s.begin());
+                    }
+                }
+            }
+        }
+        if (s.empty()) {
+            cout << "EMPTY\n";
+        }
+        else {
+            cout << s.rbegin() << ' ' << s.begin() << '\n';
+        }
+    }
+    return 0;
+}
