e95bf072586c90786638d1c99c37804434af5477
greenapple1017 <107623032+greenapple1017@users.noreply.github.com>
2025-01-06 23:28:23 +0900

    Title: 연결리스트 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250106/연결리스트 2/linked-list2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Node {
+    string str;
+    Node *prev;
+    Node *next;
+};
+
+int n;
+
+
+int main() {
+    // Please write your code here.
+    Node cur;
+    cin >> cur.str;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int cmd;
+        cin >> cmd;
+        if (cmd == 1) {
+            Node
+        }
+    }
+    return 0;
+}
