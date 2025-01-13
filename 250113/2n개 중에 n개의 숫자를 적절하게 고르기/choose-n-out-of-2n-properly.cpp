998f960e9700d4b755463965f4cb990836476891
greenapple1017 <107623032+greenapple1017@users.noreply.github.com>
2025-01-13 22:43:33 +0900

    Title: 2n개 중에 n개의 숫자를 적절하게 고르기, Time: 1035ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 250113/2n개 중에 n개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.cpp
+++ 250113/2n개 중에 n개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <climits>
+#include <algorithm>
 
 using namespace std;
 
@@ -7,21 +8,23 @@ int n;
 int num[20];
 int visited[20];
 int ans = INT_MAX;
-int sum, sum1;
+int sum, sum1, sum2;
 
 void sol(int selected) {
     if (selected == n) {
-        int diff = (sum1 > sum-sum1) ? sum1-(sum-sum1) : (sum-sum1) - sum1;
+        int diff = (sum1 > sum2) ? sum1 - sum2 : sum2 - sum1;
         ans = min(ans, diff);
         return;
     }
     for (int i = 0; i < 2 * n; i++) {
         if (!visited[i]) {
             sum1 += num[i];
+            sum2 -= num[i];
             visited[i] = 1;
             sol(selected + 1);
             visited[i] = 0;
             sum1 -= num[i];
+            sum2 += num[i];
         }
     }
 }
@@ -32,6 +35,7 @@ int main() {
         cin >> num[i];
         sum += num[i];
     }
+    sum2 = sum;
     sol(0);
     cout << ans;
 
