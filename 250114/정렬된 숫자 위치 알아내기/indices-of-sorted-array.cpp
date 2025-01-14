8a449553e954d4db1c04376961c59f9d0a1eb07a
greenapple1017 <107623032+greenapple1017@users.noreply.github.com>
2025-01-14 23:42:25 +0900

    Title: 정렬된 숫자 위치 알아내기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250114/정렬된 숫자 위치 알아내기/indices-of-sorted-array.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N;
+int ans[1000];
+
+struct Num {
+    int data;
+    int order;
+} nums[1000];
+
+struct comp {
+    bool operator()(int a, int b) const {
+        if (nums[a].data == nums[b].data) {
+            return nums[a].order < nums[b].order;
+        }
+        return nums[a].data < nums[b].data;
+    }
+}
+
+int main() {
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> nums[i].data;
+        nums[i].order = i+1;
+    }
+
+    sort(nums, nums + N, comp());
+
+    for (int i = 0; i < N; i++) {
+        ans[nums[i].order - 1] = i+1;
+    }
+
+    for (int i = 0; i < N; i++) {
+        cout << ans[i] << ' ';
+    }
+
+    // Write your code here!
+
+
+    return 0;
+}
