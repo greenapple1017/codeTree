185bc459d6df3b5bb47e2dc85dab56e59d2ca2bd
greenapple1017 <107623032+greenapple1017@users.noreply.github.com>
2025-01-16 23:57:08 +0900

    Title: 노드의 인접한 공통 조상, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250116/노드의 인접한 공통 조상/adjacent-common-ancestor-nodes.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int from[10000];
+int to[10000];
+int a, b;
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n - 1; i++) {
+        cin >> from[i] >> to[i];
+    }
+
+    cin >> a >> b;
+
+    // Write your code here!
+
+    return 0;
+}
